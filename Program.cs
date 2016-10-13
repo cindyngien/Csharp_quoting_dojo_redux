1d083c305b9ed6231b8288bf67a8f239ca0c3be7
Cindy Ngien <cindyngien@gmail.com>
2016-10-13 13:49:07 -0700

    c# quoting dojo redux. quoting site using db connection. nancy fx.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting;
+using DbConnection;
+
+namespace Quotes
+{
+    public class Program
+    {
+          public static void Create()
+          {
+            Console.WriteLine("Creating A New Quote");
+            Console.Write("Your name: ");
+            string user = Console.ReadLine();
+
+            Console.Write("Your quote: ");
+            string quote = Console.ReadLine();
+            
+            string query = $"INSERT INTO quotes (user, quote, created_at) VALUES('{user}', '{quote}', NOW())";
+            DbConnector.ExecuteQuery(query);
+            Console.WriteLine("The quote has been added to the db!");
+          }
+
+      
+        public static void Main(string[] args)
+        {  
+            // Create();
+            // Read();
+            // Destroy();
+
+            IWebHost host = new WebHostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+            host.Run();
+                
+        }
+    }
+}
